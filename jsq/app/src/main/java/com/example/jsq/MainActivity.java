e9343308a8083246b7d9483ee5abe87b42551ba6
jjj19950710 <2507343041@qq.com>
2018-04-22 23:27:56 +0800

    jisuanqi

--- jsq/app/src/main/java/com/example/jsq/MainActivity.java
+++ jsq/app/src/main/java/com/example/jsq/MainActivity.java
@@ -1,13 +1,312 @@
 package com.example.jsq;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.jsq.R;
+
+public class MainActivity extends Activity {
+
+    private Button one,two,three,four,five,six,seven,eight,nine,zero,divide,sqrt, multiply,remainder, minus, convert, digit, equal, plus,cancel;
+    private EditText editText;//显示输入的数字
+    private String opt = "+";//操作符
+    private double n1 = 0.0, n2 = 0.0;//两个操作数
+    private TextView textView;//显示算式
+    private double  num =0.0;
+
+    //跟据被选择按钮的id设置监听器
+    private OnClickListener lisenter = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            editText = (EditText)findViewById(R.id.input);
+            textView = (TextView) findViewById(R.id.out);
+            String s = editText.getText().toString();//获取字符串
+            Button btn =(Button)v;
+            try{
+
+                switch(btn.getId())
+                {
+                    case R.id.one://1
+                    {
+                        String str = editText.getText().toString();
+                        editText.setText(str + 1);
+                        str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.plus://+
+                    {
+                        String str = editText.getText().toString();
+                        n1 = Double.parseDouble(str);
+                        opt = "+";
+                        textView.setText(n1 + opt);
+                        editText.setText("");
+                        break;
+                    }
+                    case R.id.two://2
+                    {
+                        String str = editText.getText().toString();
+                        editText.setText(str + 2);
+                        str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.equal://操作符=
+                    {
+                        if(opt == "+")
+                        {
+                            String str = editText.getText().toString();
+                            n2 = Double.parseDouble(str);
+                            textView.setText(n1 + opt + n2 + "=");
+                            editText.setText((n1 + n2) + "");
+                        }
+                        else if(opt == "-")
+                        {
+                            String str = editText.getText().toString();
+                            n2 = Double.parseDouble(str);
+                            textView.setText(n1 + opt + n2 + "=");
+                            editText.setText((n1 - n2) + "");
+                        }
+                        else if(opt == "*")
+                        {
+                            String str = editText.getText().toString();
+                            n2 = Double.parseDouble(str);
+                            textView.setText(n1 + opt + n2 + "=");
+                            editText.setText((n1 * n2) + "");
+                        }
+                        else if(opt == "/")
+                        {
+                            String str = editText.getText().toString();
+                            n2 = Double.parseDouble(str);
+                            if(n2 == 0)
+                            {
+                                editText.setText("");
+                                textView.setText("除数不能为0");
+                                break;
+                            }
+                            else
+                            {
+                                textView.setText(n1 + opt + n2 + "=");
+                                editText.setText((n1 / n2) + "");
+                            }
+                        }
+
+                        break;
+                    }
+                    case R.id.three://3
+                    {
+                        editText.setText(editText.getText().toString() + 3);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.four://4
+                    {
+                        editText.setText(editText.getText().toString() + 4);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.five://5
+                    {
+                        editText.setText(editText.getText().toString() + 5);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.six://6
+                    {
+                        editText.setText(editText.getText().toString() + 6);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.seven://7
+                    {
+                        editText.setText(editText.getText().toString() + 7);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.eight://8
+                    {
+                        editText.setText(editText.getText().toString() + 8);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.nine://9
+                    {
+                        editText.setText(editText.getText().toString() + 9);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.zero://0
+                    {
+                        textView.setText(n1 + opt + 10);
+                        editText.setText(editText.getText().toString() + 0);
+                        String str = editText.getText().toString();
+                        textView.setText(str);
+                        break;
+                    }
+                    case R.id.digit://.
+                    {
+                        String str = editText.getText().toString();
+                        if(str.indexOf(".") != -1) //判断字符串中是否已经包含了小数点，如果有就什么也不做
+                        {
+
+                        }
+                        else //如果没有小数点
+                        {
+                            if(str.equals("0"))//如果开始为0，
+                                editText.setText(("0" + ".").toString());
+                            else if(str.equals(""))//如果初时显示为空，就什么也不做
+                            {
+
+                            }
+                            else
+                                editText.setText(str + ".");
+                        }
+                        break;
+                    }
+                    case R.id.divide://操作符 /
+                    {
+                        String str = editText.getText().toString();
+                        n1 = Double.parseDouble(str);
+                        opt = "/";
+                        editText.setText("");
+                        textView.setText(n1 + opt);
+                        break;
+                    }
+                    case R.id.multiply://操作符*
+                    {
+                        String str = editText.getText().toString();
+                        n1 = Double.parseDouble(str);
+                        opt = "*";
+                        editText.setText("");
+                        textView.setText(n1 + opt);
+                        break;
+                    }
+                    case R.id.minus://操作符-
+                    {
+                        String str = editText.getText().toString();
+                        n1 = Double.parseDouble(str);
+                        opt = "-";
+                        editText.setText("");
+                        textView.setText(n1 + opt);
+                        break;
+                    }
+                    case R.id.convert://genhao
+                    {
+                        String str = editText.getText().toString();
+                        n1 = Double.parseDouble(str);
+                        if(n1 < 0)
+                        {
+                            editText.setText("");
+                            textView.setText("负数没有平方根");
+                        }
+                        else
+                        {
+                            editText.setText(Math.sqrt(n1) + "");
+                            textView.setText(n1 + "的平方根是");
+                        }
+
+                        break;
+                    }
+                    case R.id.remainder://+/-
+                    {
+                        String str =editText.getText().toString();
+                        n1 = Double.parseDouble(str);
+                        if(str.length() > 0)
+                            editText.setText(-n1 + "");
+                        textView.setText(-n1 + "");
+                        break;
+                    }
+                    case R.id.cancel://CE
+                    {
+                        String str =editText.getText().toString();
+                        if(str.length() > 0)
+                            editText.setText("");
+                        break;
+                    }
+                    case R.id.sqrt://<-
+                    {
+                        String str =editText.getText().toString();
+                        if(str.length() > 0)
+                            editText.setText(str.substring(0, str.length() - 1));
+                        break;
+                    }
+
+                }
+            }catch(Exception e){}
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six= (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        zero = (Button) findViewById(R.id.zero);
+        divide = (Button) findViewById(R.id.divide);
+        sqrt = (Button) findViewById(R.id.sqrt);
+        multiply = (Button) findViewById(R.id.multiply);
+        remainder = (Button) findViewById(R.id.remainder);
+        minus = (Button) findViewById(R.id.minus);
+        convert = (Button) findViewById(R.id.convert);
+        digit = (Button) findViewById(R.id.digit);
+        equal = (Button) findViewById(R.id.equal);
+        plus = (Button) findViewById(R.id.plus);
+        cancel = (Button) findViewById(R.id.cancel);
+
+        //为按钮添加监听器
+        one.setOnClickListener(lisenter);
+        two.setOnClickListener(lisenter);
+        three.setOnClickListener(lisenter);
+        four.setOnClickListener(lisenter);
+        five.setOnClickListener(lisenter);
+        six.setOnClickListener(lisenter);
+        seven.setOnClickListener(lisenter);
+        eight.setOnClickListener(lisenter);
+        nine.setOnClickListener(lisenter);
+        zero.setOnClickListener(lisenter);
+        divide.setOnClickListener(lisenter);
+        equal.setOnClickListener(lisenter);
+        sqrt.setOnClickListener(lisenter);
+        multiply.setOnClickListener(lisenter);
+        remainder.setOnClickListener(lisenter);
+        minus.setOnClickListener(lisenter);
+        convert.setOnClickListener(lisenter);
+        digit.setOnClickListener(lisenter);
+        plus.setOnClickListener(lisenter);
+        cancel.setOnClickListener(lisenter);
     }
+
+
 }
